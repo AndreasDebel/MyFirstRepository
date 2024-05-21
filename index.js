85c8216df0063a1102be6abf6ca1b3b13bb61122
AndreasDebel <andreasdebel@hotmail.com>
2024-05-21 10:55:48 +0200

    andreasdebel.dk lagt ind

---
+++ index.js
@@ -0,0 +1,12 @@
+function buttonClick(){
+    alert("You clicked me");
+}
+
+/* const writeNameAndAge = () => {
+    let = userName = document.getElementsByName("user_name").value;
+    let = userAge = document.getElementsByName("user_age").value;
+    alert(`Your name is ${userName}, and you are ${userAge} years old.`);
+    console.log(userAge);
+}
+
+writeNameAndAge(); */
